51bae6d807b74d14abaaf12e69f468f4e73eca3e
Adam <iadamalad@gmail.com>
2020-08-12 17:40:28 -0700

    Bug fix in OAuth

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js - figure out what set of credentials to use
 if (process.env.NODE_ENV === "production") {
   //we are in production
-  module.exports("./prod");
+  module.exports = require("./prod");
 } else {
   //we are in developement - return dev keys
   module.exports = require("./dev");
